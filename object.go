e601b99eeb3421c8cfee6cf7a5c6a983e3743438
Tommie Gannert <tommie@gannert.se>
2021-10-13 12:33:26 +0200

    Add Symbols.
    This allows e.g. setting the @@iterator property on an object, turning
    it into an Iterable.

--- object.go
+++ object.go
@@ -64,6 +64,20 @@ func (o *Object) Set(key string, val interface{}) error {
 	return nil
 }
 
+// SetSymbol will set a property on the Object to a given value.
+// Supports all value types, eg: Object, Array, Date, Set, Map etc
+// If the value passed is a Go supported primitive (string, int32, uint32, int64, uint64, float64, big.Int)
+// then a *Value will be created and set as the value property.
+func (o *Object) SetSymbol(key *Symbol, val interface{}) error {
+	value, err := coerceValue(o.ctx.iso, val)
+	if err != nil {
+		return err
+	}
+
+	C.ObjectSetAnyKey(o.ptr, key.ptr, value.ptr)
+	return nil
+}
+
 // Set will set a given index on the Object to a given value.
 // Supports all value types, eg: Object, Array, Date, Set, Map etc
 // If the value passed is a Go supported primitive (string, int32, uint32, int64, uint64, float64, big.Int)
@@ -112,6 +126,12 @@ func (o *Object) Get(key string) (*Value, error) {
 	return valueResult(o.ctx, rtn)
 }
 
+// GetSymbol tries to get a Value for a given Object property key.
+func (o *Object) GetSymbol(key *Symbol) (*Value, error) {
+	rtn := C.ObjectGetAnyKey(o.ptr, key.ptr)
+	return valueResult(o.ctx, rtn)
+}
+
 // GetInternalField gets the Value set by SetInternalField for the given index
 // or the JS undefined value if the index hadn't been set.
 // Panics if given an out of range index.
@@ -121,7 +141,6 @@ func (o *Object) GetInternalField(idx uint32) *Value {
 		panic(fmt.Errorf("index out of range [%v] with length %v", idx, o.InternalFieldCount()))
 	}
 	return &Value{rtn, o.ctx}
-
 }
 
 // GetIdx tries to get a Value at a give Object index.
@@ -138,6 +157,12 @@ func (o *Object) Has(key string) bool {
 	return C.ObjectHas(o.ptr, ckey) != 0
 }
 
+// HasSymbol calls the abstract operation HasProperty(O, P) described in ECMA-262, 7.3.10.
+// Returns true, if the object has the property, either own or on the prototype chain.
+func (o *Object) HasSymbol(key *Symbol) bool {
+	return C.ObjectHasAnyKey(o.ptr, key.ptr) != 0
+}
+
 // HasIdx returns true if the object has a value at the given index.
 func (o *Object) HasIdx(idx uint32) bool {
 	return C.ObjectHasIdx(o.ptr, C.uint32_t(idx)) != 0
@@ -148,6 +173,11 @@ func (o *Object) Delete(key string) bool {
 	ckey := C.CString(key)
 	defer C.free(unsafe.Pointer(ckey))
 	return C.ObjectDelete(o.ptr, ckey) != 0
+}
+
+// DeleteSymbol returns true if successful in deleting a named property on the object.
+func (o *Object) DeleteSymbol(key *Symbol) bool {
+	return C.ObjectDeleteAnyKey(o.ptr, key.ptr) != 0
 }
 
 // DeleteIdx returns true if successful in deleting a value at a given index of the object.
