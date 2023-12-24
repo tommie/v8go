8f3b8810431b0adc4c96f9e13aecd13b5a923bb2
Tommie Gannert <tommie@gannert.se>
2023-12-24 09:48:48 +0100

    Fixes android package names.
    Copy-paste bug.

--- deps/android_arm64/vendor.go
+++ deps/android_arm64/vendor.go
@@ -1,3 +1,3 @@
-// Package linux_arm64 is required to provide support for vendoring modules
+// Package android_arm64 is required to provide support for vendoring modules
 // DO NOT REMOVE
-package linux_arm64
+package android_arm64
