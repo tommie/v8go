f44f8f6834f5ee7cc432c521950c46e006d8e3f1
Peter Strøiman <peter@stroiman.com>
2025-01-03 15:58:25 +0100

    Extract object.h and .cc

--- utils.h
+++ utils.h
@@ -1,6 +1,11 @@
+#ifndef V8GO_UTILS_H
+#define V8GO_UTILS_H
+
 #include <string>
 
 #include "deps/include/v8-primitive.h"
 
 extern const char* CopyString(std::string str);
 extern const char* CopyString(v8::String::Utf8Value& value);
+
+#endif
--- v8go.h
+++ v8go.h
@@ -116,22 +116,6 @@ extern RtnValue FunctionTemplateGetFunction(TemplatePtr ptr,
 
 const char* ExceptionGetMessageString(ValuePtr ptr);
 
-extern void ObjectSet(ValuePtr ptr, const char* key, ValuePtr val_ptr);
-extern void ObjectSetAnyKey(ValuePtr ptr, ValuePtr key, ValuePtr val_ptr);
-extern void ObjectSetIdx(ValuePtr ptr, uint32_t idx, ValuePtr val_ptr);
-extern int ObjectSetInternalField(ValuePtr ptr, int idx, ValuePtr val_ptr);
-extern int ObjectInternalFieldCount(ValuePtr ptr);
-extern RtnValue ObjectGet(ValuePtr ptr, const char* key);
-extern RtnValue ObjectGetAnyKey(ValuePtr ptr, ValuePtr key);
-extern RtnValue ObjectGetIdx(ValuePtr ptr, uint32_t idx);
-extern RtnValue ObjectGetInternalField(ValuePtr ptr, int idx);
-int ObjectHas(ValuePtr ptr, const char* key);
-int ObjectHasAnyKey(ValuePtr ptr, ValuePtr key);
-int ObjectHasIdx(ValuePtr ptr, uint32_t idx);
-int ObjectDelete(ValuePtr ptr, const char* key);
-int ObjectDeleteAnyKey(ValuePtr ptr, ValuePtr key);
-int ObjectDeleteIdx(ValuePtr ptr, uint32_t idx);
-
 extern RtnValue NewPromiseResolver(ContextPtr ctx_ptr);
 extern ValuePtr PromiseResolverGetPromise(ValuePtr ptr);
 int PromiseResolverResolve(ValuePtr ptr, ValuePtr val_ptr);
